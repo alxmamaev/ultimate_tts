11f49b2034429a467d9defdc8963d5b362b781db
Alexander Mamaev <alxmamaev@ya.ru>
2021-07-27 01:30:05 +0300

    Add multiprocessing to audio processing

--- ultimate_tts/utils/audio/processor.py
+++ ultimate_tts/utils/audio/processor.py
@@ -3,24 +3,23 @@ from catalyst.registry import REGISTRY
 from pathlib import Path
 from tqdm import tqdm
 from math import ceil
-
+import os
+from functools import partial
+from torch.multiprocessing import Pool
 
 class AudioProcessor:
-    def __init__(self, sample_rate, transforms, batch_size=1):
+    def __init__(self, transforms, batch_size=1):
         """AudioProcessor handles audios processing.
         This processor applies transforms in their order to the batch of audios.
 
         Parameters
         ----------
-        sample_rate : int
-            Which sample rate, will used to resample inputs audios.
         transforms : Union[List[Callable], List[Dict]]
             List of callable transforms objects, or their config dicts.
         batch_size : int, optional
             Batch size for data processing, by default 1
         """
 
-        self.sample_rate = sample_rate
         self.transforms = []
         self.batch_size = batch_size
 
@@ -50,17 +49,41 @@ class AudioProcessor:
 
         return audios_batch
 
+    
+    def process_batch_offline(self, sample_rate, output_path, input_paths):
+        audios_batch = []
+        for input_wav_path in input_paths:
+            audio, _sample_rate = taudio.load(str(input_wav_path))
 
-    def process_files(self, inputs, outputs, verbose=False):
+            assert sample_rate <= _sample_rate, f"Error at file {input_wav_path}, sample rate of input audio less than target sample_rate"
+
+            audio = taudio.functional.resample(audio, _sample_rate, sample_rate)
+
+            assert audio.shape[0] == 1, f"Error at file {input_wav_path}, audio must be monophonic"
+            assert audio.shape[1] > 0, f"Error at file {input_wav_path}, audio length is zero"
+
+            audio = audio[0] # Get first channel of audio
+            audios_batch.append(audio)
+
+        audios_batch = self.__call__(audios_batch)
+        
+        for input_path, audio in  zip(input_paths, audios_batch):
+            filename = input_path.stem + ".wav"
+            output_wav_path = output_path.joinpath(filename)
+
+            audio = audio.unsqueeze(0)
+            assert audio.shape[1] != 0, f"Error at file {input_wav_path}, audio length after processing is zero"
+            taudio.save(str(output_wav_path), audio, sample_rate)
+
+
+    def process_files(self, inputs, outputs, sample_rate=22050, verbose=False):
         input_metadata = []
 
         input_metadata_path = Path(inputs["metadata_path"])
         input_wavs_path = Path(inputs["wavs_path"])
         output_wavs_path = Path(outputs["wavs_path"])
-        output_metadata_path = Path(outputs["metadata_path"])
 
         output_wavs_path.mkdir(parents=True, exist_ok=True)
-        output_metadata_path.parent.mkdir(parents=True, exist_ok=True)
 
         with open(input_metadata_path) as f:
             for line in f:
@@ -70,42 +93,33 @@ class AudioProcessor:
         
 
         item_index = 0
-        if verbose:
-            print("Processing audios...")
-            progress_bar = tqdm(total=int(ceil(len(input_metadata) / self.batch_size)))
+        batched_input_paths = []
 
         while True:
-            audios_batch = []
-            filenames_batch = []
+            input_paths_batch = []
 
-            while item_index < len(input_metadata) and len(filenames_batch) < self.batch_size:
+            while item_index < len(input_metadata) and len(input_paths_batch) < self.batch_size:
                 filename, _ = input_metadata[item_index]
-
                 input_wav_path = input_wavs_path.joinpath(filename + ".wav")
-                audio, _ = taudio.load(str(input_wav_path))
-
-                assert audio.shape[0] == 1, "audio must be monophonic"
-                audio = audio[0]
-
-                audios_batch.append(audio)
-                filenames_batch.append(filename)
+                
+                input_paths_batch.append(input_wav_path)
                 item_index += 1
             
-            if not filenames_batch:
+            if not input_paths_batch:
                 break
 
-            audios_batch = self.__call__(audios_batch)
-            for filename, audio in  zip(filenames_batch, audios_batch):
-                if audio is None:
-                    continue
+            batched_input_paths.append(input_paths_batch)
+        
 
-                output_wav_path = output_wavs_path.joinpath(filename + ".wav")
-                taudio.save(str(output_wav_path), audio.unsqueeze(0), self.sample_rate)
+        process_func = partial(self.process_batch_offline, sample_rate, output_wavs_path)
+        nproc = os.cpu_count()
+
+        with Pool(nproc) as p:
+            tasks = p.imap_unordered(process_func, batched_input_paths)
 
             if verbose:
-                progress_bar.update(1)
+                print("Start audio processing")
+                tasks = tqdm(tasks, total=len(batched_input_paths))
 
-
-        with open(output_metadata_path, "w") as f:
-            for filename, text in input_metadata:
-                f.write(f"{filename}|{text}\n")
+            for _ in tasks:
+                continue
